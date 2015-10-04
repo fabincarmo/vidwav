b3134437668282c3c39e7343597cd68efbd75a88
fabincarmo <fabincarmo[[@gmail.com]]>
2015-10-04 11:17:49 -0300

    v0.1

---
+++ vidwav.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+@author: fabincarmo@gmail.com
+"""
+
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
+
+# Uncomment if you want to use TeX
+#from matplotlib import rc
+#rc('text', usetex=True)
+#rc('font', **{'family':'serif', 'serif':['Computer Modern Roman'], 
+#        'monospace': ['Computer Modern Typewriter'], 'size':8})
+
+import matplotlib.animation as manimation
+import wave
+
+from matplotlib.ticker import FuncFormatter
+def kilo(x, pos):
+    return '%1.fk' % (x*1e-3)
+
+def vidwav(wavfile, fps=25):
+
+    FFMpegWriter = manimation.writers['ffmpeg']
+    metadata = dict(title='Wav Spectrogram', artist='Matplotlib',
+            comment='')
+    writer = FFMpegWriter(fps=fps, metadata=metadata, bitrate=6000)
+    
+    wf = wave.open(wavfile, 'rb')
+    
+    fs = wf.getframerate()
+    N = wf.getnframes()
+    duration = N/float(fs)
+    bytes_per_sample = wf.getsampwidth()
+    bits_per_sample  = bytes_per_sample * 8
+    dtype = 'int{0}'.format(bits_per_sample)
+    channels = wf.getnchannels()
+    
+    audio = np.fromstring(wf.readframes(int(duration*fs*bytes_per_sample/channels)), dtype=dtype)
+#    audio /= float(np.max(np.abs(audio)))
+    audio.shape = (audio.shape[0]/channels, channels)
+    audio_fft = np.fft.fft(audio[:,0])
+    freqs = np.fft.fftfreq(audio[:,0].shape[0], 1.0/fs) / 1000.0
+    max_freq_kHz = freqs.max()
+    times = np.arange(audio.shape[0]) / float(fs)
+    fftshift = np.fft.fftshift
+    
+    fig = plt.figure(figsize=(4,3))
+    
+    plt.subplot(211)
+    plt.plot(times, (audio[:,0]).astype(float)/np.max(np.abs(audio[:,0])), c='k', lw=.5)
+    plt.xlim(0,duration)
+    plt.ylim(-1,1)
+    
+    l1, = plt.plot([], [], '#333333', lw=2)
+    
+    plt.subplot(212)
+    plt.specgram(audio[:,0], Fs=fs)
+    plt.xlim(0,duration)
+    plt.ylim(0,max_freq_kHz*1000.0)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    formatter = FuncFormatter(kilo)
+    ax = plt.gca()
+    ax.yaxis.set_major_formatter(formatter)
+    
+    l2, = plt.plot([], [], '#333333', lw=2)
+    
+    plt.tight_layout()
+    
+    x = np.array([0., 0.])
+    y0 = np.array([-1, 1])
+    y1 = np.array([0, max_freq_kHz*1000.0])
+    
+    with writer.saving(fig, "temp.mp4", 100):
+        for i in range((int(duration)+1)*fps):
+            x += 1.0/float(fps)
+            l1.set_data(x,y0)
+            l2.set_data(x,y1)
+            writer.grab_frame()
+    
+    import os
+    os.system("ffmpeg -y -i "+wavfile+" -i temp.mp4 "+wavfile.split('.')[0]+".mp4")
+    os.system("rm temp.mp4")
+
+def main():
+    vidwav("data.wav")
+
+if __name__ == "__main__":
+    main()
